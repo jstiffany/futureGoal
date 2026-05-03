cac18484d97908ac9fd6b7e1f1f0371f430ed71a
jstiffany <191784353+jstiffany@users.noreply.github.com>
2026-05-03 19:24:35 -0400

    doing a LOT of work i see

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
+const path = require('path');
+
 const { app, BrowserWindow } = require("electron");
+
+try {
+  require('electron-reloader')(module);
+} catch (_) {}
+
+const additionalData = { myKey: 'futureGoal' }
+const gotTheLock = app.requestSingleInstanceLock(additionalData)
+
+if (!gotTheLock) {
+  app.quit() // If a second one tries to start, kill it immediately
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
+    // If someone tries to run a second instance, focus on the main window instead
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+let mainWindow;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 400,
+    width: 470,
     height: 617,
+    x: 1950,
+    y: 225,
     resizable: false,
     maximizable: false,
     fullscreenable: false,
     frame: false, 
     transparent: false,
     webPreferences: {
-      contextIsolation: true
+      contextIsolation: true,
+      nodeIntegration: true
     }
   });
 
-  win.loadFile("mainPage/index.html");
+  win.loadFile(path.join(__dirname, 'mainPage', 'index.html'));
 }
 
 app.whenReady().then(createWindow);
